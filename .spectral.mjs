223046a2adf15c5144204da30bba1dff23a2c3d9
ana.hernandez@stoplight.io <ana.hernandez@stoplight.io>
2023-07-10 11:51:47 -0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwOTg2MQ/spectral.js?branch_slug=master&token=45830e85-3ce9-43d9-b85f-5cafc08ba2a1";
+export default { extends: ruleset };
